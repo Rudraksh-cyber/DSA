0b8657df76e7a371df6f6256e897c7345b80b0b5
Rudraksh-cyber <rudrakshgautam97@gmail.com>
2026-06-30 21:26:01 +0530

    commit

---
+++ src/recursion/print1ton.java
@@ -0,0 +1,13 @@
+class Main {
+    static void printNumbers(int i, int n){
+        if (i > n) {
+            return;
+        }
+        System.out.println(i);
+        printNumbers(i + 1, n);
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        printNumbers(1, n);
+    }
+}
