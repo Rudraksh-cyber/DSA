090d687e876372d140106e33b886a290b1513491
Rudraksh-cyber <rudrakshgautam97@gmail.com>
2026-07-08 11:54:03 +0530

    commit

---
+++ src/Sorting/MergeSort.java
@@ -0,0 +1,52 @@
+package Sorting;
+
+class Solution {
+
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int mid = (low + high) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+    public static void merge(int[] arr, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int left = low;
+        int right = mid + 1;
+        int index = 0;
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp[index] = arr[left];
+                left++;
+            } else {
+                temp[index] = arr[right];
+                right++;
+            }
+            index++;
+        }
+
+        while (left <= mid) {
+            temp[index] = arr[left];
+            left++;
+            index++;
+        }
+        while (right <= high) {
+            temp[index] = arr[right];
+            right++;
+            index++;
+        }
+        for (int i = 0; i < temp.length; i++) {
+            arr[low + i] = temp[i];
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {6, 3, 9, 5, 2, 8};
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.println("Sorted Array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
