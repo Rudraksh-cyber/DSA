900fe9f6a38522a16d08869bf3b0c4d62071160d
Rudraksh-cyber <rudrakshgautam97@gmail.com>
2026-07-19 14:43:24 +0530

    commit

---
+++ src/array/SortedAndRotated.java
@@ -0,0 +1,19 @@
+package array;
+public class SortedAndRotated {
+    public boolean check(int[] nums) {
+        int breaks = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                breaks++;
+
+                if (breaks > 1) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
