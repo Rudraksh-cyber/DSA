a2ba06e387195e504d05bc2255b976f786469d36
Rudraksh-cyber <rudrakshgautam97@gmail.com>
2026-07-20 23:53:59 +0530

    RemovingFromSortedArray

---
+++ src/array/RemovingFromSortedArray.java
@@ -0,0 +1,18 @@
+package array;
+public class RemovingFromSortedArray{
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int index = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        return index;
+    }
+}
