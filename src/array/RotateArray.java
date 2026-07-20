605b48db38d2e46744232251a773aa4cc1284f87
Rudraksh-cyber <rudrakshgautam97@gmail.com>
2026-07-20 23:58:00 +0530

    Rotate Array

---
+++ src/array/RotateArray.java
@@ -0,0 +1,18 @@
+package array;
+public class RotateArray{
+    public void leftRotate(int[] nums) {
+        int n = nums.length;
+
+        if (n <= 1) {
+            return;
+        }
+
+        int first = nums[0];
+
+        for (int i = 0; i < n - 1; i++) {
+            nums[i] = nums[i + 1];
+        }
+
+        nums[n - 1] = first;
+    }
+}
